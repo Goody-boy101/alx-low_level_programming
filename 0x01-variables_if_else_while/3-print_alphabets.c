e3bf467f495c5802009477f8e874b40b834315b8
Goody-boy101 <nazzyigwegbe@gmail.com>
2023-07-15 12:55:56 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -4,24 +4,25 @@
  *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-        char c;
- 
- 	char d;
-        
+	char c;
+
+	char d;
+
 	c = 'a';
-        d = 'A';
-        while
-                (c <= 'z') {
-                        putchar(c);
-                        c++;
-                }
-        while
-                (d <= 'Z') {
-                        putchar(d);
-                        d++;
-                }
-        putchar('\n');
-        return (0);
+	d = 'A';
+	while
+		(c <= 'z') {
+			putchar(c);
+			c++;
+		}
+	while
+		(d <= 'Z') {
+			putchar(d);
+			d++;
+		}
+	putchar('\n');
+	return (0);
 }
