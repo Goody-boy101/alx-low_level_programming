760547fcdfb022c11569d8471033f010fb5c732b
Goody-boy101 <nazzyigwegbe@gmail.com>
2023-08-24 19:42:07 -0700

    1-list_len.c

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -2,10 +2,10 @@
 #include "lists.h"
 
 /**
- * custom_list_len - Returns the number of elements in a linked list
- * @head: pointer to the list_t list
+ * custom_list_len - Returns the number of elements in a linked list.
+ * @head: pointer to the list_t list.
  *
- * Return: number of elements in the linked list
+ * Return: number of elements in the linked list.
  */
 size_t custom_list_len(const list_t *head)
 {
