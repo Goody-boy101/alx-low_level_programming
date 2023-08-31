36491616f29ad42a2588c0cc014e059ad3d085c0
Goody-boy101 <nazzyigwegbe@gmail.com>
2023-08-31 15:05:15 -0700

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -13,14 +13,14 @@ unsigned int binary_to_uint(const char *b)
 
 	int i = 0;
 
-	while (b[i] != '\0')
+	while (b[i] == '0' || b[i] == '1')
 	{
-		if (b[i] != '0' && b[i] != '1')
-		return (0);
+		dec_val = dec_val * 2 + (b[i] - '0');
 
-		dec_val = (dec_val << 1) + (b[i] - '0');
 		i++;
 	}
+	if (b[i] != '\0')
+		return (0);
 
 	return (dec_val);
 }
