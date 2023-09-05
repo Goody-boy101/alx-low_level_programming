2d7a2649b8173c061e3a789648e9278b834b8680
Goody-boy101 <nazzyigwegbe@gmail.com>
2023-09-04 18:24:44 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,10 +12,10 @@ int create_file(const char *filename, char *text_content)
 {
 	int fd, w, len = 0;
 
-	if (!filename)
+	if (filename == NULL)
 		return (-1);
 
-	if (text_content)
+	if (text_content != NULL)
 	{
 		len = strlen(text_content);
 	}
