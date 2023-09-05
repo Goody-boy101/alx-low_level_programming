a87da44365573bda543c0accbcc839328a80784b
Goody-boy101 <nazzyigwegbe@gmail.com>
2023-09-04 18:05:04 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -21,7 +21,7 @@ int create_file(const char *filename, char *text_content)
 			len++;
 	}
 
-	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
 
@@ -36,5 +36,6 @@ int create_file(const char *filename, char *text_content)
 	}
 
 	close(fd);
+
 	return (1);
 }
